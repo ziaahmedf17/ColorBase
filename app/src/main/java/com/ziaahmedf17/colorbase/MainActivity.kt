aac334888f8e9a8e001a1e277469ebfe4919e3ab
Zia Ahmed <ziaahmedf17@gmail.com>
2023-08-09 04:49:34 +0500

    Initial commit

---
+++ app/src/main/java/com/ziaahmedf17/colorbase/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.ziaahmedf17.colorbase
+
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.ziaahmedf17.colorbase.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        setListners()
+    }
+
+    private fun setListners() {
+        val clickableViews: List<View> = listOf(
+            binding.boxOneText, binding.boxTwoText,
+            binding.boxThreeText, binding.boxFourText,
+            binding.boxFiveText, binding.constrainLayout,
+            binding.redButton, binding.greenButton, binding.yellowButton
+        )
+        for (item in clickableViews) {
+            item.setOnClickListener {
+                makeColored(it)
+            }
+        }
+    }
+
+    private fun makeColored(view: View) {
+        when (view.id) {
+            R.id.box_one_text -> view.setBackgroundColor(Color.DKGRAY)
+            R.id.box_two_text -> view.setBackgroundColor(Color.GRAY)
+            R.id.box_three_text -> view.setBackgroundResource(android.R.color.holo_green_light)
+            R.id.box_four_text -> view.setBackgroundResource(android.R.color.holo_green_dark)
+            R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_blue_bright)
+
+            R.id.red_button -> binding.boxThreeText.setBackgroundResource(R.color.my_red)
+            R.id.yellow_button -> binding.boxFourText.setBackgroundResource(R.color.my_yellow)
+            R.id.green_button -> binding.boxFiveText.setBackgroundResource(R.color.my_green)
+
+            else -> view.setBackgroundColor(Color.LTGRAY)
+        }
+    }
+}
